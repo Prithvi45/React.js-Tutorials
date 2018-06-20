309467b8b63d14591bf03f073433550f29db97cb
Prithvi45 <maneprithviraj@sggs.ac.in>
2018-06-20 21:43:43 +0530

    playing around components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
-    // return (
-    //   <div className="App">
-    //     <h1> Hi , i am Prithviraj ! </h1>
-    //   </div>
-    // );
-    return React.createElement('div',{className : 'App'},React.createElement('h1',null,'Hi i\'m Prithviraj!'));
-  }
-}
+  	return (
+       <div className="App">
+       <h2> Hi , i am Prithviraj</h2>
+       <Person />
+
+       </div>
+      );
+  };
+};
 
 
 export default App;
---
+++ my-app/src/Person/Person.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const person = () => {
+	return <p>I am a Developer and i have  { Math.floor(Math.random() * 10) } years of experience</p>;
+};
+
+export default person;
