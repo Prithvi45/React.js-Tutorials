d87ee0c71c8532c580f27408afa78f7baf45f93c
Prithvi45 <maneprithviraj@sggs.ac.in>
2018-06-21 16:57:21 +0530

    playing around components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,12 +4,33 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+	state = {
+		persons:[
+        {name :'Prithviraz', age:23 },
+        {name: 'raj',age:24}
+		]
+	}
+
+	switchNameHandler =() =>{
+		this.setState ( 
+		{
+			persons:[
+            {name :'Updated Prithviraz', age:23 },
+            {name: 'updated raj',age:24}
+		    ]
+		}
+		)
+	};
   render() {
   	return (
        <div className="App">
        <h2> Hi , i am Prithviraj</h2>
        <Person name="Prithvi" age="23" />
+
+       <button onClick={this.switchNameHandler}>Switch Name</button>
        <Person name="Prithviraj" age="24"> My hobbies : watching cricket</Person>
+       <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+       <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
 
        </div>
       );
