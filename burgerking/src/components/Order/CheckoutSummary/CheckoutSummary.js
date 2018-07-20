62fd3956e7a031e35c4ef465876838ba5fafdd18
Prithvi45 <maneprithviraj@sggs.ac.in>
2018-07-21 00:50:03 +0530

    routers added to burgerking

--- burgerking/src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ burgerking/src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -1,28 +1,24 @@
 import React from 'react';
+
 import Burger from '../../Burger/Burger';
-import Button from '../../UI/Button/Button'
-import classes from './CheckoutSummary.css'
+import Button from '../../UI/Button/Button';
+import classes from './CheckoutSummary.css';
 
 const checkoutSummary = (props) => {
     return (
         <div className={classes.CheckoutSummary}>
-            <h1>We hope it tastes  well !</h1>
-            <div style={{width:'100%', height:'auto', margin:'auto'}}>
+            <h1>We hope it tastes well!</h1>
+            <div style={{width: '100%', margin: 'auto'}}>
                 <Burger ingredients={props.ingredients}/>
             </div>
-                <Button 
+            <Button 
                 btnType="Danger"
-                clicked={props.checkoutCancelled} >CANCEL</Button>
-
-                <Button 
+                clicked={props.checkoutCancelled}>CANCEL</Button>
+            <Button 
                 btnType="Success"
-                clicked={props.checkoutContinued} >CONTINUE</Button>
-
-            </div>
-    )
-
-};
-
-
+                clicked={props.checkoutContinued}>CONTINUE</Button>
+        </div>
+    );
+}
 
 export default checkoutSummary;
